1d43a54bfe2c836181e59b804c5515d623385d36
Kosei Moriyama <cou929@gmail.com>
2013-12-21 13:13:22 +0900

    support path

--- app.rb
+++ app.rb
@@ -1,21 +1,32 @@
 require 'sinatra'
 
-get '/' do
+get '/*' do
+  mode = params['mode'] =~ /^(?:get|set|clear)$/ ? params['mode'] : 'get'
+  send(mode)
+end
+
+def get
   @cookies = request.cookies
   erb :cookies
 end
 
-get %r{/set(?:\.([a-z]+))?} do |format|
+def set
   @set_cookies = []
+  reserved_keys = ['path', 'mode', 'format']
+  path = params['path'] || request.path
+  format = params['format']
+
   params.each do |key, value_len|
+    next if reserved_keys.any? {|k| k == key}
     next if value_len.is_a? Enumerable
     next if value_len.to_i == 0
     next if value_len.to_i > 100000
     value = ''
     value_len.to_i.times{value  << (65 + rand(25)).chr}
-    response.set_cookie(key, {:value => value, :expires => Time.new(Time.new().year + 1, 1, 1)})
+    response.set_cookie(key, {:value => value, :expires => Time.new(Time.new().year + 1, 1, 1), :path => path})
     @set_cookies.push({'key' => key, 'value' => value})
   end
+
   if format == 'gif'
     content_type 'image/gif'
     "GIF89a\1\0\1\0%c\0\0%c%c%c\0\0\0,\0\0\0\0\1\0\1\0\0%c%c%c\1\0;" % [144, 153, 0, 0, 2, 2, 4]
@@ -24,7 +35,7 @@ get %r{/set(?:\.([a-z]+))?} do |format|
   end
 end
 
-get '/clear' do
+def clear
   @cookies = request.cookies
   request.cookies.each do |key, value|
     response.set_cookie(key, {:value => '', :expires => Time.new(2000,1,1)})
